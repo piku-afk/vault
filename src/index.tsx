6218a775ce5b210923a4fe5fd136d223369b0bcf
Piyush Mahato <51166024+piku-afk@users.noreply.github.com>
2025-08-03 15:05:05 +0530

    feat: integrate mantine ui

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,18 @@
+import { MantineProvider } from '@mantine/core';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router';
+
+import '@fontsource-variable/inter';
+import '@mantine/core/styles.css';
+
 import { router } from './router.ts';
+import { theme } from './theme.ts';
 
 createRoot(document.getElementById('root') as HTMLDivElement).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <MantineProvider theme={theme}>
+      <RouterProvider router={router} />
+    </MantineProvider>
   </StrictMode>
 );
---
+++ src/theme.ts
@@ -0,0 +1,9 @@
+import { createTheme } from '@mantine/core';
+
+const fontFamily = 'Inter Variable, sans-serif';
+
+export const theme = createTheme({
+  fontFamily,
+  headings: { fontFamily },
+  primaryColor: 'violet',
+});
