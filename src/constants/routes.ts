a738d2982de6dbcd443287c0f11381fbcce15a51
Piyush Mahato <51166024+piku-afk@users.noreply.github.com>
2025-08-06 00:31:17 +0530

    feat: implement authentication flow with login page, update routes, and enhance UI with Mantine components

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -1,6 +1,6 @@
 export enum ROUTES {
   HOME = '/',
-  SIGN_IN = '/sign-in',
+  SIGN_IN = '/login',
   TWO_FACTOR = '/session/two-factor',
   INVESTMENTS = '/investments',
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,26 @@
 import netlifyPlugin from '@netlify/vite-plugin-react-router';
 import { reactRouter } from '@react-router/dev/vite';
-import autoprefixer from 'autoprefixer';
+import mantinePostcss from 'postcss-preset-mantine';
+import postcssSimpleVars from 'postcss-simple-vars';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   css: {
-    postcss: { plugins: [autoprefixer()] },
+    postcss: {
+      plugins: [
+        mantinePostcss,
+        postcssSimpleVars({
+          variables: {
+            'mantine-breakpoint-xs': '36em',
+            'mantine-breakpoint-sm': '48em',
+            'mantine-breakpoint-md': '62em',
+            'mantine-breakpoint-lg': '75em',
+            'mantine-breakpoint-xl': '88em',
+          },
+        }),
+      ],
+    },
   },
   plugins: [reactRouter(), tsconfigPaths(), netlifyPlugin()],
   server: {
