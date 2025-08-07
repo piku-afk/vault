f38bd8ee808ad00a8d85fd93a068733de5b10f8b
Piyush Mahato <51166024+piku-afk@users.noreply.github.com>
2025-08-07 10:44:47 +0530

    feat: update Supabase client initialization to use process.env variables

--- app/utils/supabase.server.ts
+++ app/utils/supabase.server.ts
@@ -5,8 +5,8 @@ export function createClient(request: Request) {
   const headers = new Headers();
 
   const supabase = createServerClient<Database>(
-    import.meta.env.VITE_SUPABASE_URL,
-    import.meta.env.VITE_SUPABASE_ANON_KEY,
+    process.env.SUPABASE_URL as string,
+    process.env.SUPABASE_ANON_KEY as string,
     {
       cookies: {
         getAll() {
