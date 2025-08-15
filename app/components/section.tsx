965ebfa56a9ff1c32ebd23e2d2087b175a64277b
Piyush Mahato <51166024+piku-afk@users.noreply.github.com>
2025-08-15 10:35:40 +0530

    refactor: enhance Section component structure and smooth scrolling behavior

--- app/components/section.tsx
+++ app/components/section.tsx
@@ -1,13 +1,33 @@
-import { Box, Title } from "@mantine/core";
-import type { PropsWithChildren } from "react";
+import { Anchor, Box, Title } from "@mantine/core";
+import { type PropsWithChildren, useEffect } from "react";
 
-export function Section(props: PropsWithChildren & { title?: string }) {
+export function Section(props: PropsWithChildren & { title: string }) {
+  const sectionId = props.title.toLowerCase().replace(/\s+/g, "-");
+
+  useEffect(() => {
+    // Check if the current URL hash matches this section's ID
+    if (window.location.hash === `#${sectionId}`) {
+      const element = document.getElementById(sectionId);
+      if (element) {
+        element.scrollIntoView({ behavior: "smooth" });
+      }
+    }
+  }, [sectionId]);
+
   return (
     <Box component="section">
       {props.title && (
-        <Title order={2} mb={props.children ? "lg" : 0} fw="normal">
-          {props.title}
-        </Title>
+        <Anchor c="black" underline="never" href={`#${sectionId}`}>
+          <Title
+            id={sectionId}
+            order={2}
+            size="h3"
+            mb={props.children ? "md" : 0}
+            fw={500}
+          >
+            {props.title}
+          </Title>
+        </Anchor>
       )}
 
       {props.children}
